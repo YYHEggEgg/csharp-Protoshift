2100118a410332896b6e253098bc31ba8d57db9f
EggEgg <53960525+YYHEggEgg@users.noreply.github.com>
2023-02-01 09:14:40 +0800

    Created

---
+++ csharp-Protoshift/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace csharp_Protoshift
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
