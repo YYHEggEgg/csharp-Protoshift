0f6b0812df260a1e60d730fcb390f985c9add8ad
YYHEggEgg <53960525+YYHEggEgg@users.noreply.github.com>
2023-02-06 13:27:38 +0800

    Support RSA Operations (Powered By Openssl)

---
+++ csharp-Protoshift/Tools.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_Protoshift
+{
+    internal static class Tools
+    {
+        public static string ProgramPath => AppDomain.CurrentDomain.BaseDirectory;
+
+        static Random ran = new Random();
+
+        /// <summary>
+        /// Generate a random string with length of [len]. 
+        /// </summary>
+        public static string RandomStr(int len)
+        {
+            string charset = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890";
+            string res = "";
+            while (len-- > 0)
+            {
+                res += charset[ran.Next(0, 61)];
+            }
+            return res;
+        }
+
+        public async static Task ExecuteProcess(string path, string args)
+        {
+            var p = Process.Start(path, args);
+            await p.WaitForExitAsync();
+        }
+    }
+}
