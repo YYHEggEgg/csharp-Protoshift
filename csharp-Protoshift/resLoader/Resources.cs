ff65aee2c8a15fb671d05457d71a8e8e574e7d82
YYHEggEgg <53960525+YYHEggEgg@users.noreply.github.com>
2023-02-06 14:14:26 +0800

    Added KcpProxy description

---
+++ csharp-Protoshift/resLoader/Resources.cs
@@ -0,0 +1,17 @@
+﻿using csharp_Protoshift.RSA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_Protoshift.resLoader
+{
+    public static class Resources
+    {
+        public static byte[] dispatchKey = new byte[] { };
+        public static byte[] dispatchSeed = new byte[] { };
+        public static Dictionary<uint, Openssl> CPri = new();
+        public static Dictionary<uint, Openssl> SPub = new();
+    }
+}
