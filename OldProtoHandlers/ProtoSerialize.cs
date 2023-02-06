9a6e48ba206b0d459d3104824f94474be719baff
YYHEggEgg <53960525+YYHEggEgg@users.noreply.github.com>
2023-02-06 17:57:33 +0800

    Added 3.3 GIO protos

---
+++ OldProtoHandlers/ProtoSerialize.cs
@@ -0,0 +1,56 @@
+﻿using Google.Protobuf;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OldProtos
+{
+    public class ProtoSerialize
+    {
+#pragma warning disable CS8600 // 将 null 字面量或可能为 null 的值转换为非 null 类型。
+#pragma warning disable CS8601 // 引用类型赋值可能为 null。
+#pragma warning disable CS8602 // 解引用可能出现空引用。
+#pragma warning disable CS8604 // 引用类型参数可能为 null。W
+        public ProtoSerialize(string protoname) : this(Type.GetType(protoname)) { }
+
+        public ProtoSerialize(Type prototype)
+        {
+            var parser_pro = prototype.GetProperty("Parser", BindingFlags.Static | BindingFlags.Public);
+            var parse_get = parser_pro.GetGetMethod();
+            Parser = (MessageParser)parse_get.Invoke(null, null);
+            Parser = Parser.WithDiscardUnknownFields(true);
+
+            isNull = false;
+        }
+#pragma warning restore CS8600 // 将 null 字面量或可能为 null 的值转换为非 null 类型。
+#pragma warning restore CS8601 // 引用类型赋值可能为 null。
+#pragma warning restore CS8602 // 解引用可能出现空引用。
+#pragma warning restore CS8604 // 引用类型参数可能为 null。
+
+        public MessageParser Parser;
+
+        private ProtoSerialize() 
+        {
+            Parser = WindSeedClientNotify.Parser; // Fuck
+            isNull = true;
+        }
+        public readonly bool isNull;
+        public static ProtoSerialize Empty { get => new(); }
+
+        public string DeserializeToJson(byte[] protobin)
+        {
+            if (isNull) throw new InvalidOperationException("Trying to use an invalid instance.");
+            return JsonFormatter.Default.Format(Parser.ParseFrom(protobin));
+        }
+
+        public byte[] SerializeFromJson(string protojson)
+        {
+            if (isNull) throw new InvalidOperationException("Trying to use an invalid instance.");
+            return MessageExtensions.ToByteArray(Parser.ParseJson(protojson));
+        }
+    }
+}
---
+++ OldProtoHandlers/QueryCmdId.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OldProtos
+{
+    public static class QueryCmdId
+    {
+        public static string ThisPath => AppDomain.CurrentDomain.BaseDirectory;
+        private static Dictionary<int, ProtoSerialize> serializers = new();
+        private static Dictionary<string, int> protonameToCmdids = new();
+
+        static QueryCmdId()
+        {
+            var cmdids = File.ReadAllLines("/resources/protobuf/oldcmdid.csv");
+            foreach (var line in cmdids)
+            {
+                var csvline = line.Split(',');
+                Debug.Assert(csvline.Length == 2);
+                string name = csvline[0].Trim();
+                int cmdid = int.Parse(csvline[1].Trim());
+                serializers.Add(cmdid, new ProtoSerialize(name));
+                protonameToCmdids.Add(name, cmdid);
+            }
+        }
+
+        public static bool TryGetSerializer(int cmdid, out ProtoSerialize serializer)
+        {
+            if (serializers.ContainsKey(cmdid))
+            {
+                serializer = serializers[cmdid];
+                return true;
+            }
+            else
+            {
+                serializer = ProtoSerialize.Empty;
+                return false;
+            }
+        }
+
+        public static bool TryGetSerializer(string protoname, out ProtoSerialize serializer)
+        {
+            if (protonameToCmdids.ContainsKey(protoname))
+            {
+                serializer = serializers[protonameToCmdids[protoname]];
+                return true;
+            }
+            else
+            {
+                serializer = ProtoSerialize.Empty;
+                return false;
+            }
+        }
+    }
+}
