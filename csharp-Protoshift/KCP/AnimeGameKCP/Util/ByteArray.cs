5aad6768c6d6dc45335f624314b5132bee188913
YYHEggEgg <53960525+YYHEggEgg@users.noreply.github.com>
2023-02-07 16:22:11 +0800

    Fixed ByteArray (wtf YSFreedom used)

--- csharp-Protoshift/KCP/AnimeGameKCP/Util/ByteArray.cs
+++ csharp-Protoshift/KCP/AnimeGameKCP/Util/ByteArray.cs
@@ -6,35 +6,67 @@ namespace YSFreedom.Common.Util
 {
     public static class ByteArray
     {
-        public static void SetUInt16(this byte[] arr, int offset, ushort value, bool bigEndian = false)
+        public static void SetUInt16(this byte[] arr, int offset, ushort value)
         {
-            if (bigEndian) value = (ushort)IPAddress.HostToNetworkOrder((short)value);
-            arr[offset + 0] = (byte)(value & 0xFF);
-            arr[offset + 1] = (byte)((value & 0xFF00) >> 8);
+            arr[offset + 1] = (byte)(value & 0xFF);
+            arr[offset + 0] = (byte)((value & 0xFF00) >> 8);
         }
 
-        public static void SetUInt32(this byte[] arr, int offset, uint value, bool bigEndian = false)
+        public static void SetUInt32(this byte[] arr, int offset, uint value)
         {
-            if (bigEndian) value = (uint)IPAddress.HostToNetworkOrder((int)value);
-            arr[offset + 0] = (byte)(value & 0xFF);
-            arr[offset + 1] = (byte)((value & 0xFF00) >> 8);
-            arr[offset + 2] = (byte)((value & 0xFF0000) >> 16);
-            arr[offset + 3] = (byte)((value & 0xFF000000) >> 24);
+            arr[offset + 3] = (byte)(value & 0xFF);
+            arr[offset + 2] = (byte)((value & 0xFF00) >> 8);
+            arr[offset + 1] = (byte)((value & 0xFF0000) >> 16);
+            arr[offset + 0] = (byte)((value & 0xFF000000) >> 24);
         }
 
-        public static ushort GetUInt16(this byte[] arr, int offset, bool bigEndian = false)
+        public static void SetUInt64(this byte[] arr, int offset, ulong value)
         {
-            ushort ret = (ushort)((ushort)arr[offset + 0] | ((ushort)arr[offset + 1] << 8));
-            if (bigEndian) ret = (ushort)IPAddress.NetworkToHostOrder((short)ret);
+            arr[offset + 7] = (byte)(value & 0xFF);
+            arr[offset + 6] = (byte)((value & 0xFF00) >> 8);
+            arr[offset + 5] = (byte)((value & 0xFF0000) >> 16);
+            arr[offset + 4] = (byte)((value & 0xFF000000) >> 24);
+            arr[offset + 3] = (byte)((value & 0xFF00000000) >> 32);
+            arr[offset + 2] = (byte)((value & 0xFF0000000000) >> 40);
+            arr[offset + 1] = (byte)((value & 0xFF000000000000) >> 48);
+            arr[offset + 0] = (byte)((value & 0xFF00000000000000) >> 56);
+        }
+
+        public static ushort GetUInt16(this byte[] arr, int offset)
+        {
+            ushort ret = (ushort)((ushort)arr[offset + 1] | ((ushort)arr[offset + 0] << 8));
             return ret;
         }
 
-        public static uint GetUInt32(this byte[] arr, int offset, bool bigEndian = false)
+        public static uint GetUInt32(this byte[] arr, int offset)
         {
-            uint ret = ((uint)arr[offset + 0] | ((uint)arr[offset + 1] << 8) |
-                         ((uint)arr[offset + 2] << 16) | ((uint)arr[offset + 3] << 24));
-            if (bigEndian) ret = (uint)IPAddress.NetworkToHostOrder((int)ret);
+            uint ret = ((uint)arr[offset + 3] | ((uint)arr[offset + 2] << 8) |
+                         ((uint)arr[offset + 1] << 16) | ((uint)arr[offset + 0] << 24));
             return ret;
+        }
+
+        public static ulong GetUInt64(this byte[] arr, int offset)
+        {
+            ulong ret = ((ulong)arr[offset + 7]) | ((ulong)arr[offset + 6] << 8) |
+                         ((ulong)arr[offset + 5] << 16) | ((ulong)arr[offset + 4] << 24) |
+                         ((ulong)arr[offset + 3] << 32) | ((ulong)arr[offset + 2] << 40) |
+                         ((ulong)arr[offset + 1] << 48) | ((ulong)arr[offset + 0] << 56);
+            return ret;
+        }
+
+        public static byte[] Fill0(this byte[] arr, int toLength)
+        {
+            if (arr.Length < toLength)
+            {
+                var rtn = new byte[toLength];
+                Array.Copy(arr, 0, rtn, 0, arr.Length);
+                for(int i = arr.Length; i < toLength; i++)
+                {
+                    rtn[i] = 0;
+                }
+                return rtn;
+            }
+            return arr;
         }
 
         public static string HexDump(this byte[] bytes, int bytesPerLine = 16)
