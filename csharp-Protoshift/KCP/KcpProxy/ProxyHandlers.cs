852d2617a1339eaa1358053c4d751770e65bf251
EggEgg <53960525+YYHEggEgg@users.noreply.github.com>
2023-03-15 00:01:55 +0800

    supported fault packet destroy

--- csharp-Protoshift/KCP/KcpProxy/ProxyHandlers.cs
+++ csharp-Protoshift/KCP/KcpProxy/ProxyHandlers.cs
@@ -1,7 +1,7 @@
 namespace csharp_Protoshift.KcpProxy
 {
-    public Func<byte[], uint, byte[]>? OnServerPacketArrival;
-    public Func<byte[], uint, byte[]>? OnClientPacketArrival;
+    public Func<byte[], uint, byte[]?>? OnServerPacketArrival;
+    public Func<byte[], uint, byte[]?>? OnClientPacketArrival;
     public Func<byte[], uint, bool>? IsUrgentServerPacket;
     public Func<byte[], uint, bool>? IsUrgentClientPacket;
 }
