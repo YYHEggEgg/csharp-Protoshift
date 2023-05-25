b41e6b9ba0d45365b065922245af438152580f02
miHoMoSpy <sh_yang027@126.com>
2023-05-25 21:35:21 +0800

    Added base code

---
+++ KcpTests/KcpPerformanceTest/Constants.cs
@@ -0,0 +1,9 @@
+namespace csharp_Protoshift.MhyKCP.Test.App
+{
+    public static class Constants
+    {
+        public const ushort UDP_CLIENT_PORT = 30331;
+        public const ushort UDP_SERVER_PORT = 30351;
+        public const ushort UDP_PROXY_PORT = 30341;
+    }
+}
---
+++ KcpTests/KcpPerformanceTest/Server/ServerApp.cs
@@ -0,0 +1,10 @@
+namespace csharp_Protoshift.MhyKCP.Test.App
+{
+    public static class ServerApp
+    {
+        public static async Task Start()
+        {
+            
+        }
+    }
+}
