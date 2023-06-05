02c7d93e3fa374d36ea64e22ba562ea7a75f6c0b
miHoMoSpy <sh_yang027@126.com>
2023-06-05 12:00:57 +0800

    changed test standard

--- KcpTests/KcpPerformanceTest/Constants.cs
+++ KcpTests/KcpPerformanceTest/Constants.cs
@@ -5,12 +5,12 @@ namespace csharp_Protoshift.MhyKCP.Test.App
         public const ushort UDP_SERVER_PORT = 30351;
         public const ushort UDP_PROXY_PORT = 30341;
 
-        public const int each_packet_size = 500;
+        public const int each_packet_size = 3500;
         // 是否随机包大小 最小为0 最大为each_packet_size
         public const bool random_packet_size = false;
         // 发包总数量
-        public const int packet_repeat_time = 10;
+        public const int packet_repeat_time = 5000;
         // 发包间隔 非严格间隔，即发完包后等待毫秒数
-        public const int packet_interval_ms = 500;
+        public const int packet_interval_ms = 80;
     }
 }
