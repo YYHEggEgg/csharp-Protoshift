a949c0b633b13fc042161d1351f57f3f007f0a72
EggEgg <53960525+YYHEggEgg@users.noreply.github.com>
2023-06-22 20:19:44 +0800

    Test 4: Methos = UnSafeSegManager (lock), Interval = 10, Repeat = 10000, Size = 15000

--- KcpTests/KcpPerformanceTest/Constants.cs
+++ KcpTests/KcpPerformanceTest/Constants.cs
@@ -5,14 +5,14 @@ namespace csharp_Protoshift.MhyKCP.Test.App
         public const ushort UDP_SERVER_PORT = 30351;
         public const ushort UDP_PROXY_PORT = 30341;
 
-        public const int each_packet_size = 3500;
+        public const int each_packet_size = 15000;
         // 是否随机包大小 最小为0 最大为each_packet_size
         // 目前包大小固定
         // public const bool random_packet_size = false;
         // 发包总数量
         public const int packet_repeat_time = 10000;
         // 发包间隔 非严格间隔，即发完包后等待毫秒数
-        public const int packet_interval_ms = 5;
+        public const int packet_interval_ms = 10;
         // 在Actions上跑要分配true 决定是否发包结束后10s退出 是否输出packet.log到控制台
         public const bool running_on_github_actions = true;
     }
