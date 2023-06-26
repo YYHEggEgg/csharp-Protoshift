5ba8368222f9dc946002d41350a04de45ee7154c
EggEgg <53960525+YYHEggEgg@users.noreply.github.com>
2023-06-26 11:42:36 +0000

    Added benchmark project

---
+++ ProtoshiftBenchmark/Tests/GetMapFields.cs
@@ -0,0 +1,28 @@
+namespace csharp_Protoshift.Enhanced.Benchmark
+{
+    internal static class GetMapFields
+    {
+        public static NewProtos.ChannelerSlabBuffSchemeInfo GetUInt32AndUInt32(int repeat)
+        {
+            NewProtos.ChannelerSlabBuffSchemeInfo rtn = new();
+            for (uint i = 0; i < repeat; i++)
+            {
+                rtn.SlotMap.Add(i ^ 824323094, i ^ 295420825);
+            }
+            return rtn;
+        }
+    }
+
+    internal static class GetMapFieldsVerity
+    {
+        public static OldProtos.ChannelerSlabBuffSchemeInfo GetUInt32AndUInt32(int repeat)
+        {
+            OldProtos.ChannelerSlabBuffSchemeInfo rtn = new();
+            for (uint i = 0; i < repeat; i++)
+            {
+                rtn.SlotMap.Add(i ^ 824323094, i ^ 295420825);
+            }
+            return rtn;
+        }
+    }
+}
