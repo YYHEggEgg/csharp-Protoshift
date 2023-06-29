fe22661fa4be084a385ba723ae187ef421d8c8a0
YYHEggEgg <53960525+YYHEggEgg@users.noreply.github.com>
2023-06-30 00:58:24 +0800

    critical bugfix

--- ProtoshiftHandlers/SpecialHandlers/HandlerUnionCmd.cs
+++ ProtoshiftHandlers/SpecialHandlers/HandlerUnionCmd.cs
@@ -20,7 +20,7 @@ namespace csharp_Protoshift.Enhanced.Handlers.GeneratedCode
             if (newprotocol == null) return oldprotocol;
             try
             {
-                if (oldprotocol.MessageId != 0 && !oldprotocol.Body.IsEmpty)
+                if (newprotocol.MessageId != 0 && !newprotocol.Body.IsEmpty)
                 {
                     oldprotocol.MessageId = ShiftCmdId.NewShiftToOld(newprotocol.MessageId);
                     oldprotocol.Body = ProtoshiftDispatch.NewShiftToOld(
@@ -72,13 +72,13 @@ namespace csharp_Protoshift.Enhanced.Handlers.GeneratedCode
         public override byte[] NewShiftToOld(ReadOnlySpan<byte> span)
             => NewShiftToOld(newproto_parser_base.ParseFrom(span)).ToByteArray();
         public override ByteString NewShiftToOld(ByteString bytes)
-            => NewShiftToOld(newproto_parser_base.ParseFrom(bytes).ToByteString());
+            => NewShiftToOld(newproto_parser_base.ParseFrom(bytes)).ToByteString();
         public override byte[] OldShiftToNew(byte[] arr, int offset, int length)
             => OldShiftToNew(oldproto_parser_base.ParseFrom(arr, offset, length)).ToByteArray();
         public override byte[] OldShiftToNew(ReadOnlySpan<byte> span)
             => OldShiftToNew(oldproto_parser_base.ParseFrom(span)).ToByteArray();
         public override ByteString OldShiftToNew(ByteString bytes)
-            => OldShiftToNew(oldproto_parser_base.ParseFrom(bytes).ToByteString());
+            => OldShiftToNew(oldproto_parser_base.ParseFrom(bytes)).ToByteString();
 
         private static HandlerUnionCmd _globalOnlyInstance = new HandlerUnionCmd();
         public static HandlerUnionCmd GlobalInstance => _globalOnlyInstance;
