355c4e03fee7ac95cbc19af2363dd1ff4491e18b
YYHEggEgg <53960525+YYHEggEgg@users.noreply.github.com>
2023-07-11 20:27:36 +0800

    Added curr command

---
+++ csharp-Protoshift/Commands/Dispatch/CurrRegionCmd.cs
@@ -0,0 +1,39 @@
+﻿using csharp_Protoshift.Commands.Dispatch;
+using csharp_Protoshift.resLoader;
+using Google.Protobuf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextCopy;
+using YYHEggEgg.Logger;
+
+namespace csharp_Protoshift.Commands
+{
+    internal class CurrRegionCmd : ICommandHandler
+    {
+        public string CommandName => "dcurr";
+
+        public string Description => "Decrypt curr regions with Client Private keys.";
+
+        public string Usage => $"dcurr <key_id> <curr_file>{Environment.NewLine}" +
+            $"Due to some reasons, you should place the curr json into a file and give the file path here.";
+
+        public void CleanUp()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task HandleAsync(string[] args)
+        {
+            uint key_id = uint.Parse(args[0]);
+            var curr = CurrExtend.GetCurrFromJson(
+                await File.ReadAllTextAsync(args[1]), Resources.CPri[key_id]);
+            var res = JsonFormatter.Default.Format(curr);
+            Log.Info($"Decrypted json content: \n{res}", nameof(CurrRegionCmd));
+            await ClipboardService.SetTextAsync(res);
+            Log.Info("Result copied to clipboard!", nameof(CurrRegionCmd));
+        }
+    }
+}
