27f0a95472f57345b84142f399e0342cadccd689
YYHEggEgg <53960525+YYHEggEgg@users.noreply.github.com>
2023-07-25 21:15:30 +0800

    Fixed dcurr verification

--- csharp-Protoshift/resLoader/Resources.cs
+++ csharp-Protoshift/resLoader/Resources.cs
@@ -7,6 +7,7 @@ namespace csharp_Protoshift.resLoader
         public static byte[] dispatchKey = new byte[] { };
         public static byte[] dispatchSeed = new byte[] { };
         public static Dictionary<uint, RSAUtilBase> CPri = new();
+        public static Dictionary<uint, RSAUtilBase> SPub = new();
         public static Dictionary<uint, RSAUtilBase> SPri = new();
     }
 }
