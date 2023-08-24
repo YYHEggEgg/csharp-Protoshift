6035c58ef702b1b89b53fd6d54ca9146b6db6bfa
YYHEggEgg <53960525+YYHEggEgg@users.noreply.github.com>
2023-08-24 23:29:18 +0800

    updated benchmark command line

--- ProtoshiftBenchmark/run-benchmark.sh
+++ ProtoshiftBenchmark/run-benchmark.sh
@@ -1,1 +1,1 @@
-dotnet run --configuration Release --property:DefineConstants=TRACE%3bRELEASE%3bNET%3bNET6_0%3bNETCOREAPP%3bMIHOMO_KCP%3bPROTOSHIFT_BENCHMARK
+dotnet run --configuration Release --property:DefineConstants=TRACE%3bRELEASE%3bNET%3bNET6_0%3bNETCOREAPP%3bMIHOMO_KCP%3bPROTOSHIFT_BENCHMARK -- --extra-property "DefineConstants=TRACE%3bRELEASE%3bNET%3bNET6_0%3bNETCOREAPP%3bMIHOMO_KCP%3bPROTOSHIFT_BENCHMARK" "$@"
