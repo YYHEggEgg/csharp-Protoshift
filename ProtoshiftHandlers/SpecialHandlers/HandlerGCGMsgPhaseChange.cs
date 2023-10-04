d429ce1ec9ecaf3069f53ff2cc937abeaa171a8b
YYHEggEgg <53960525+YYHEggEgg@users.noreply.github.com>
2023-10-04 23:24:14 +0800

    Basic GCG special shifting

---
+++ ProtoshiftHandlers/SpecialHandlers/HandlerGCGMsgPhaseChange.cs
@@ -0,0 +1,102 @@
+using csharp_Protoshift.Enhanced.Handlers.CustomHandlers;
+using csharp_Protoshift.ProtoHotPatch;
+using Google.Protobuf;
+using Google.Protobuf.Collections;
+using Google.Protobuf.Reflection;
+using System.Collections.Generic;
+
+namespace csharp_Protoshift.Enhanced.Handlers.GeneratedCode
+{
+    public class HandlerGCGMsgPhaseChange 
+        : HandlerBase<NewProtos.GCGMsgPhaseChange, OldProtos.GCGMsgPhaseChange>
+    {
+        #region Base Type
+        MessageParser<NewProtos.GCGMsgPhaseChange> newproto_parser_base = NewProtos.GCGMsgPhaseChange.Parser;
+        MessageParser<OldProtos.GCGMsgPhaseChange> oldproto_parser_base = OldProtos.GCGMsgPhaseChange.Parser;
+        #endregion
+        #region Import Types
+        HandlerGCGPhaseType handler_GCGPhaseType = HandlerGCGPhaseType.GlobalInstance;
+        HandlerUint32PairAndMap handler_fuck = HandlerUint32PairAndMap.GlobalInstance;
+        #endregion
+
+        #region Protocol Shift
+        [System.Diagnostics.DebuggerNonUserCode]
+        [System.CodeDom.Compiler.GeneratedCode("YYHEggEgg/csharp_Protoshift.HandlerGenerator", "1.0.1.0")]
+        public override OldProtos.GCGMsgPhaseChange? NewShiftToOld(NewProtos.GCGMsgPhaseChange? newprotocol)
+        {
+            if (newprotocol == null) return null;
+            OldProtos.GCGMsgPhaseChange oldprotocol = new();
+            handler_fuck.NewShiftToOld(newprotocol.AllowControllerList, oldprotocol.AllowControllerMap);
+            oldprotocol.AfterPhase = handler_GCGPhaseType.NewShiftToOld(newprotocol.AfterPhase);
+            oldprotocol.BeforePhase = handler_GCGPhaseType.NewShiftToOld(newprotocol.BeforePhase);
+            return oldprotocol;
+        }
+
+        [System.Diagnostics.DebuggerNonUserCode]
+        [System.CodeDom.Compiler.GeneratedCode("YYHEggEgg/csharp_Protoshift.HandlerGenerator", "1.0.1.0")]
+        public override NewProtos.GCGMsgPhaseChange? OldShiftToNew(OldProtos.GCGMsgPhaseChange? oldprotocol)
+        {
+            if (oldprotocol == null) return null;
+            NewProtos.GCGMsgPhaseChange newprotocol = new();
+            newprotocol.AfterPhase = handler_GCGPhaseType.OldShiftToNew(oldprotocol.AfterPhase);
+            newprotocol.BeforePhase = handler_GCGPhaseType.OldShiftToNew(oldprotocol.BeforePhase);
+            handler_fuck.OldShiftToNew(oldprotocol.AllowControllerMap, newprotocol.AllowControllerList);
+            return newprotocol;
+        }
+        #endregion
+
+        [System.Diagnostics.DebuggerNonUserCode]
+        [System.CodeDom.Compiler.GeneratedCode("YYHEggEgg/csharp_Protoshift.HandlerGenerator", "1.0.1.0")]
+        public override byte[] NewShiftToOld(byte[] arr, int offset, int length)
+        {
+            var rtn = NewShiftToOld(newproto_parser_base.ParseFrom(arr, offset, length));
+            return rtn == null ? Array.Empty<byte>() : rtn.ToByteArray();
+        }
+        [System.Diagnostics.DebuggerNonUserCode]
+        [System.CodeDom.Compiler.GeneratedCode("YYHEggEgg/csharp_Protoshift.HandlerGenerator", "1.0.1.0")]
+        public override byte[] NewShiftToOld(ReadOnlySpan<byte> span)
+        {
+            var rtn = NewShiftToOld(newproto_parser_base.ParseFrom(span));
+            return rtn == null ? Array.Empty<byte>() : rtn.ToByteArray();
+        }
+        [System.Diagnostics.DebuggerNonUserCode]
+        [System.CodeDom.Compiler.GeneratedCode("YYHEggEgg/csharp_Protoshift.HandlerGenerator", "1.0.1.0")]
+        public override ByteString NewShiftToOld(ByteString bytes)
+        {
+            var rtn = NewShiftToOld(newproto_parser_base.ParseFrom(bytes));
+            return rtn == null ? ByteString.Empty : rtn.ToByteString();
+        }
+        [System.Diagnostics.DebuggerNonUserCode]
+        [System.CodeDom.Compiler.GeneratedCode("YYHEggEgg/csharp_Protoshift.HandlerGenerator", "1.0.1.0")]
+        public override byte[] OldShiftToNew(byte[] arr, int offset, int length)
+        {
+            var rtn = OldShiftToNew(oldproto_parser_base.ParseFrom(arr, offset, length));
+            return rtn == null ? Array.Empty<byte>() : rtn.ToByteArray();
+        }
+        [System.Diagnostics.DebuggerNonUserCode]
+        [System.CodeDom.Compiler.GeneratedCode("YYHEggEgg/csharp_Protoshift.HandlerGenerator", "1.0.1.0")]
+        public override byte[] OldShiftToNew(ReadOnlySpan<byte> span)
+        {
+            var rtn = OldShiftToNew(oldproto_parser_base.ParseFrom(span));
+            return rtn == null ? Array.Empty<byte>() : rtn.ToByteArray();
+        }
+        [System.Diagnostics.DebuggerNonUserCode]
+        [System.CodeDom.Compiler.GeneratedCode("YYHEggEgg/csharp_Protoshift.HandlerGenerator", "1.0.1.0")]
+        public override ByteString OldShiftToNew(ByteString bytes)
+        {
+            var rtn = OldShiftToNew(oldproto_parser_base.ParseFrom(bytes));
+            return rtn == null ? ByteString.Empty : rtn.ToByteString();
+        }
+
+        private static HandlerGCGMsgPhaseChange _globalOnlyInstance = new HandlerGCGMsgPhaseChange();
+        [System.Diagnostics.DebuggerNonUserCode]
+        [System.CodeDom.Compiler.GeneratedCode("YYHEggEgg/csharp_Protoshift.HandlerGenerator", "1.0.1.0")]
+        public static HandlerGCGMsgPhaseChange GlobalInstance => _globalOnlyInstance;
+
+        #region Inner Messages
+        #endregion
+
+        #region Inner Enums
+        #endregion
+    }
+}
