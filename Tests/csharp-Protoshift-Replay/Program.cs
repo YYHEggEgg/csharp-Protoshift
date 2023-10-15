6f7c7ea03be82d126d22cead43e11fdc6cf2a8d5
YYHEggEgg <53960525+YYHEggEgg@users.noreply.github.com>
2023-10-15 10:10:26 +0800

    Support load config for Debug Replay

--- Tests/csharp-Protoshift-Replay/Program.cs
+++ Tests/csharp-Protoshift-Replay/Program.cs
@@ -1,4 +1,5 @@
-﻿using csharp_Protoshift.Enhanced.Handlers.GeneratedCode;
+﻿using csharp_Protoshift.Configuration;
+using csharp_Protoshift.Enhanced.Handlers.GeneratedCode;
 using csharp_Protoshift.resLoader;
 using OfficeOpenXml;
 using YYHEggEgg.Logger;
@@ -22,6 +23,38 @@ namespace csharp_Protoshift.Debug.Replay
             await ResourcesLoader.Load();
 
             ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            #region Config
+            bool configLoadSucc = true;
+            var _conflog = Log.GetChannel("Configuration");
+            try
+            {
+                _conflog.LogInfo($"Loading config...");
+                await Config.InitializeAsync("./../../csharp-Protoshift/config.json");
+            }
+            catch (Exception ex)
+            {
+                _conflog.LogWarnTrace(ex, $"config.json initialize failed.");
+                configLoadSucc = false;
+            }
+
+            var conf_validate_errs = await Config.ValidateAsync();
+            if (conf_validate_errs != null && conf_validate_errs.Count > 0)
+            {
+                configLoadSucc = false;
+                _conflog.LogWarn($"ValidateAsync config.json by schema failed. Detected errors below:");
+                foreach (var err in conf_validate_errs)
+                {
+                    _conflog.LogWarn(err.ToString());
+                }
+            }
+
+            if (!configLoadSucc)
+            {
+                _conflog.LogErro("Config load failed. Please check the errors and fix them.");
+                Environment.Exit(50);
+            }
+            #endregion
 
             Log.Info("Start loading all protos, it will take some time...", "Entry");
             Log.Info(OldProtos.QueryJsonSerializer.Initialize(), "OldProtos");
