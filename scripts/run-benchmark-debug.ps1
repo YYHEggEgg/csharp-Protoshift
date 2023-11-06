82fe24bf66674c4dba7a3cc024df199d6445a854
miHomoSpy <53960525+YYHEggEgg@users.noreply.github.com>
2023-11-06 21:02:33 +0800

    Benchmark scripts (win) recover to correct path

--- scripts/run-benchmark-debug.ps1
+++ scripts/run-benchmark-debug.ps1
@@ -7,12 +7,12 @@ if ($build_exitcode -eq 0)
     Write-Host MSBuild finished. Running...
     Write-Host "Please, attach a debugger now. Project is named as 'ProtoshiftBenchmark'."
     dotnet run --no-build --configuration Debug --property:DefineConstants=TRACE%3bDEBUG%3bNET%3bNET6_0%3bNETCOREAPP%3bMIHOMO_KCP%3bPROTOSHIFT_BENCHMARK -- --extra-property "DefineConstants=TRACE%3bDEBUG%3bNET%3bNET6_0%3bNETCOREAPP%3bMIHOMO_KCP%3bPROTOSHIFT_BENCHMARK" $args
-    cd ..
+    cd ..\..
     exit $LastExitCode
 }
 else
 {
     Write-Host MSBuild failed with exitcode $build_exitcode.
-    cd ..
+    cd ..\..
     exit $build_exitcode
 }
--- scripts/run-benchmark.ps1
+++ scripts/run-benchmark.ps1
@@ -5,12 +5,12 @@ if ($build_exitcode -eq 0)
 {
     Write-Host MSBuild finished. Running...
     dotnet run --no-build --configuration Release --property:DefineConstants=TRACE%3bRELEASE%3bNET%3bNET6_0%3bNETCOREAPP%3bMIHOMO_KCP%3bPROTOSHIFT_BENCHMARK -- --extra-property "DefineConstants=TRACE%3bRELEASE%3bNET%3bNET6_0%3bNETCOREAPP%3bMIHOMO_KCP%3bPROTOSHIFT_BENCHMARK" $args
-    cd ..
+    cd ..\..
     exit $LastExitCode
 }
 else
 {
     Write-Host MSBuild failed with exitcode $build_exitcode.
-    cd ..
+    cd ..\..
     exit $build_exitcode
 }
